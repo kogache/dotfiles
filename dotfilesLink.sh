9b91d0394e6bda6e8c39165819c114adb58b582b
h.koga.orso <h_koga@orso.jp>
2017-07-05 11:44:40 +0900

    mv dotfilesLink.sh.

---
+++ dotfilesLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.netrwhist ~/.vim
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
